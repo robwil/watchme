5bf596762ef7e25ee856b8b4a1e82616b1abf49d
Rob Williams <robwilliams42@gmail.com>
2014-01-22 15:26:37 -0500

    Stop printing imgur response because it is circular reference for JSON.stringify

--- server/app.js
+++ server/app.js
@@ -86,7 +86,8 @@ app.post('/photos', function(request, response) {
 				var deleteHash = reply.split("||")[1];
 				var agent = superagent.agent();
 				agent.del("https://api.imgur.com/3/image/" + deleteHash).set("Authorization", "Client-ID " + IMGUR_CLIENT_ID).end(function(imgurResponse) {
-					console.log("IMGUR response = " + JSON.stringify(imgurResponse));
+					//console.log("IMGUR response = " + JSON.stringify(imgurResponse));
+					console.log("IMGUR response success flag = " + imgurResponse.body.success);
 					response.statusCode = (imgurResponse.body.success ? 200 : 500);
 					response.end();
 				});
